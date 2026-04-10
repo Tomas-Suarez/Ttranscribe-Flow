ec046ea3e756e635da9d79e495269d9fb28f0bf1
Tomas Suarez <tomasa.suarez81@gmail.com>
2026-04-10 18:45:52 -0300

    Initial commit

---
+++ src/test/java/com/dev/transcribeflow/TranscribeflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.transcribeflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranscribeflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
